29ccf2c117c5ba9e88b2ebb522cd891f7d885a50
Mohammad Issawi <2000issawi@gmail.com>
2023-03-04 13:01:31 +0300

    Format main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,12 @@
 #include <App.h>
 using namespace std;
 using namespace aw;
-int main(int argc, char **argv)
-{
-    try
-    {
-        if (APP.init(argc, argv))
-            APP.loop();
-            return 0;
-    }
-    catch (const exception &err)
-    {
-        printf("An exception occured: %s", err.what());
-    }
+int main(int argc, char **argv) {
+  try {
+    if (APP.init(argc, argv))
+      APP.loop();
+    return 0;
+  } catch (const exception &err) {
+    printf("An exception occured: %s", err.what());
+  }
 }
