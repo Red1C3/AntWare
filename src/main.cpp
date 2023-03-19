902bed51903a573d1f4503185466746400ec92d3
Mohammad Issawi <2000issawi@gmail.com>
2023-03-19 23:36:05 +0300

    Change default resolution and add vita exit calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <App.h>
+#include <psp2/kernel/processmgr.h>
 using namespace std;
 using namespace aw;
 int main(int argc, char **argv) {
   try {
-    if (APP.init(argc, argv))
+    if (APP.init(argc, argv)) {
       APP.loop();
+      APP.terminate();
+    }
+    sceKernelExitProcess(0);
     return 0;
   } catch (const exception &err) {
     printf("An exception occured: %s", err.what());
   }
+  sceKernelExitProcess(0);
 }
